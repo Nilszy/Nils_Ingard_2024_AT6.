5e72edbb612d2259587b04ddcfa92e2312bfca13
Nils Ingard <nilsingard1@hotmail.no>
2024-11-30 22:39:33 +0100

    Updating from commit 3 to commit 2.

--- Nils_Ingard_2024_AT6/CreateInstances.py
+++ Nils_Ingard_2024_AT6/CreateInstances.py
@@ -1,12 +1,6 @@
 from Question5 import UserInputValidator
 
-test_list1 = ["15", "-15", "1", "20", "abc", "543"]
-test_list2 = ["99", "20", "ab", "-11", "-abe"]
-
 instance1 = UserInputValidator()
-valid_integers1 = instance1.validate_positive_integers(test_list1)
-instance1.validation_message(valid_integers1)
+
 
 instance2 = UserInputValidator()
-valid_integers2 = instance2.validate_positive_integers(test_list2)
-instance2.validation_message(valid_integers2)
